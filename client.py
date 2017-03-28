96d58c866a1ae821d3c1a4d9278dcec8eb94cb79
kjempelodott <kjempelodott@gmail.com>
2017-03-28 23:33:05 +0200

    hsauyfwgjjvgwq

--- client.py
+++ client.py
@@ -1,44 +1,42 @@
 import socket
 import json
-
+from ghostly import Map, Player, ProteusV
 
 RUN = False
 
 if __name__ == '__main__':
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((127.0.0.1, 54321))
-    s.send('NAME kjempelodott\n')
+    s.connect(('127.0.0.1', 54321))
+    s.send(b'NAME kjempelodott\n')
     
-    data = ''
+    data = b''
 
-    while '\n' not in data:
+    while b'\n' not in data:
         data += s.recv(4096)
-    welcome, data = data.split('\n', 1)
-    js = json.loads(welcome) 
+    welcome, data = data.split(b'\n', 1)
+    js = json.loads(welcome.decode())
     gamemap = Map(**js['map'])
-    ai = Proteus(**js['you'])
-    ai.set_map(gamemap)
+    ai = ProteusV(gamemap, **js['you'])
     
     while 1:
-        while '\n' not in data:
+        while b'\n' not in data:
             data += s.recv(4096)
-        *states, data = data.split('\n')
+        *states, data = data.split(b'\n')
         js = msg = None
 
         for state in states:
-            js = json.loads(state)
+            js = json.loads(state.decode())
             msg = js['messagetype']
             if msg in ['dead', 'endofround']:
                 RUN = False
             elif msg == 'startofround':
-                for other in js['others']:
-                    ai.set_other(**other)
+                ai.startround()
                 RUN = True
 
         # Use last update only
         if RUN and msg == 'stateupdate':
-            gamemap.update(js['map']['content'])
-            for other in js['others']:
-                ai.set_other(**other)
-
+            gamemap.update(js['gamestate']['map']['content'])
+            ai.update_self(**js['gamestate']['you'])
+            ai.observe(js['gamestate']['others'])
+            s.send(b'%s\n' % ai.move())
             
